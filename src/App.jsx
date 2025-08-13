ec7b236ca596c4527fabbfa9815fceb1b6106f8a
Leandro Poles <37784837+leandropoles@users.noreply.github.com>
2025-08-12 22:10:13 -0300

    Adiciona src/App.jsx (estrutura inicial)

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Delivery App Frontend</h1>
+      <p>Bem-vindo ao seu novo projeto React!</p>
+    </div>
+  );
+}
+
+export default App;
